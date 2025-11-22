3c45a7d7d74eef28b9b9ec377898d1b806c564f9
Seb Hall <greenman891@hotmail.com>
2025-11-22 16:02:50 +0000

    changed the dictionary and themes/types to be smaller and clearer

--- src/data/dictionaryService.ts
+++ src/data/dictionaryService.ts
@@ -1,10 +1,37 @@
-// --- Define the types that the rest of the app will use ---
-// --- Define the types ---
-//export type WordType = "adverbs" | "adjectives" | "verbs" | "nouns" | "interjections" | "prepositions" | "conjunctions";
-//export type WordTheme = "Animals" | "Food" | "Nature" | "Numbers" | "School" | "Emotions" | "Music" | "Body" | "Books" | "Calendar" | "Colours" | "Transportation" | "Technology" | "Weather" | "Sports" | "Clothing" ;
-export type WordTheme = "Nature" | "Weather" | "Flora" | "Fauna" | "Insects" | "Anatomy" | "Perception" | "Health" | "Food" | "Beverages" | "Clothing" | "Housing" | "Tools" | "Technology" | "Science" | "Mathematics" | "Measurement" | "Energy" | "Materials" | "Shapes" | "Time" | "Space" | "Movement" | "Travel" | "Geography" | "Economy" | "Finance" | "Occupations" | "Law" | "Politics" | "Warfare" | "Spirituality" | "Emotions" | "Personality" | "Relationships" | "Etiquette" | "Culture" | "Language" | "Education" | "Philosophy" | "Literature" | "Art" | "Music" | "Performance" | "Sports" | "Recreation" | "Humor" | "Imagination" | "Mythology" | "Danger" | "Safety" | "Death" | "Growth" | "Aging" | "Beauty" | "Strength" | "Weakness" | "Morality" | "Corruption" | "Conflict" | "Cooperation" | "Size" | "Quantity" | "Change" | "Causality" | "Probability" | "Truth" | "Mystery" | "Hope" | "Freedom" | "Ownership" | "Position" | "Existence" | "Abstraction" | "Objects"
-// export const TYPES = ["noun", "verb", "adjective", "adverb", "pronoun", "determiner", "preposition", "conjunction", "interjection"];
-export type WordType = "noun" | "verb" | "adjective" | "adverb" | "pronoun" | "determiner" | "preposition" | "conjunction" | "interjection";
+export type WordTheme =
+  | "Weather"
+  | "Tools"
+  | "Time"
+  | "Sports"
+  | "Space"
+  | "Shapes"
+  | "Science"
+  | "Relationships"
+  | "Philosophy"
+  | "Personality"
+  | "Occupations"
+  | "Nature"
+  | "Mythology"
+  | "Music"
+  | "Movement"
+  | "Mathematics"
+  | "Materials"
+  | "Literature"
+  | "Language"
+  | "Insects"
+  | "Health"
+  | "Geography"
+  | "Food"
+  | "Flora"
+  | "Fauna"
+  | "Emotions"
+  | "Education"
+  | "Conflict"
+  | "Clothing"
+  | "Beverages"
+  | "Art"
+  | "Anatomy";
+export type WordType = "noun" | "verb" | "adjective" | "adverb";
 export interface WordData {
   theme?: WordTheme[]; // plural, now an array
   type?: WordType[];   // plural, now an array
@@ -16,7 +43,7 @@ export let dictionary: Map<string, WordData> = new Map();
 
 // --- The Initializer ---
 let hasInitialized = false;
-const DICTIONARY_URL = 'https://pdsx74vla44vq2mf.public.blob.vercel-storage.com/scrabdle-dictionary2-LGI86WUt0K3Q4Cq1as8fhzmcsVMgDg.json';
+const DICTIONARY_URL = 'https://pdsx74vla44vq2mf.public.blob.vercel-storage.com/scrabdledictionary2.json';
 
 export async function initializeDictionary(): Promise<void> {
   if (hasInitialized) return;
